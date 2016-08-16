17530eabb0a5ebc1c2bd73db75e7ec289cb3b321
Terry <terry@terrytilley.com>
2016-08-16 15:38:53 +0100

    display local storage messages

--- note-controller.js
+++ note-controller.js
@@ -1,6 +1,15 @@
-var newNoteButton = document.getElementById('save-note-button');
+window.onload = function() {
+  var newNoteButton = document.getElementById('save-note-button');
 
-newNoteButton.onclick = function() {
-  var message = document.getElementById('note-message').value;
-  localStorage.setItem('message', message);
+  newNoteButton.onclick = function() {
+    var message = document.getElementById('note-message').value;
+    localStorage.setItem('message', message);
+  };
+
+  function loadMessages() {
+    var messages = localStorage.getItem('message');
+
+    document.getElementById('messages').innerHTML = messages;
+  }
+  loadMessages();
 };

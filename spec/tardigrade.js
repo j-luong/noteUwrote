05d5f7015dbc60dc40beda781349e85771ca7adc
sus111 <sgraham535@gmail.com>
2016-08-16 11:01:42 +0100

    test colors

--- spec/tardigrade.js
+++ spec/tardigrade.js
@@ -1,23 +1,36 @@
 (function(exports){
-  function Tardigrade(){}
+  var colors = require('colors');
+  function Tardigrade(){
+    space = "   ";
+  }
 
   Tardigrade.prototype = {
+    describe: function(title, testFunction){
+      console.log(title);
+      testFunction();
+    },
+
+    it: function(title, testFunction){
+      console.log(space + title);
+      testFunction(title);
+    },
+
     isTrue: function(assertionToCheck) {
       if (!assertionToCheck) {
-        throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+        console.log((space.repeat(2) + "Assertion failed: " + assertionToCheck + " is not truthy").red);
       } else {
-        console.log(".");
+        console.log(space.repeat(2) + "passed".green);
       }
     },
 
     isFalse: function(assertionToCheck) {
       if (assertionToCheck) {
-        throw new Error("Assertion failed: " + assertionToCheck + " is not falsey");
+        throw new Error((space.repeat(2) + "Assertion failed: " + assertionToCheck + " is not falsey").red);
       } else {
-        console.log(".");
+        console.log(space.repeat(2) + "passed".green);
       }
     }
-  };
+  },
 
   exports.tardigrade = Tardigrade;
 })(this);

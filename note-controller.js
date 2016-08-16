cdffaa8c1fe10cfbb6fea570e95918c80182b9d2
Terry <terry@terrytilley.com>
2016-08-16 15:12:23 +0100

    Save to local storage

--- note-controller.js
+++ note-controller.js
@@ -1,1 +1,6 @@
-document.getElementById("app").innerHTML = "Howdy";
+var newNoteButton = document.getElementById('save-note-button');
+
+newNoteButton.onclick = function() {
+  var message = document.getElementById('note-message').value;
+  localStorage.setItem('message', message);
+};
